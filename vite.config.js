a9c9080448790c27b7800262da3ac95cd4b065fe
ousttrue <ousttrue@gmail.com>
2023-11-22 20:48:06 +0900

    vite

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import fs from 'fs'
+import path from "path";
+
+export default {
+  server: {
+    https: {
+      key: fs.readFileSync('./localhost-key.pem'),
+      cert: fs.readFileSync('./localhost.pem'),
+    }
+  },
+}
